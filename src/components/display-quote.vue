bbecb7672e40c1f5059d6859fbdf2b640b0f9a1a
tanmayc07 <tanmayc32@gmail.com>
2020-09-24 11:15:33 +0530

    :sparkles: Fetch quote from api

---
+++ src/components/display-quote.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="quote">
+    <p>Hello {{ name }}, your quote for today is: "{{ quote }}"</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "display-quote",
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    quote: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+.quote {
+  height: 100%;
+  width: 200px;
+  background-color: rgb(251, 170, 170);
+  color: rgb(5, 5, 5);
+  border-radius: 5px;
+  padding: 20px;
+}
+</style>
